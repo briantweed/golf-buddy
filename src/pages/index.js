3a662b04c1cca37f52f6312bad7c2bed5dd15e6f
Brian Tweed <brian.tweed@ioausa.com>
2025-12-09 22:05:12 +0000

    init commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -36,12 +36,13 @@ const Page = () => {
     return (
         <main className={"min-h-screen w-screen text-xl text-grey-6 bg-grey-7"}>
 
-            <section className={"pt-4 pb-8 px-4"}>
-                <h1 className={"text-2xl mb-2"}>{testGame.CourseName} </h1>
-                <h2 className={"text-base"}>
-                    <i>Par: <span className={"ml-2"}>{courseParScore}</span></i> | <i>Total Yards:<span className={"ml-2"}> {courseTotalYards}</span></i>
+            <section className={"flex justify-between p-4"}>
+                <h1 className={"text-2xl font-medium"}>{testGame.CourseName} </h1>
+                <h2 className={"text-base mt-1"}>
+                    <i>Par: <span className={"ml-2"}>{courseParScore}</span></i>
                 </h2>
-                <div className="flex gap-1 text-base mt-4">
+                <h2 className={"text-base mt-1"}><i>Total Yards:<span className={"ml-2"}> {courseTotalYards}</span></i></h2>
+                <div className="flex gap-1 text-base mt-1">
                     <span><i>Number of Holes:</i></span>
                     <span className={"text-sm ml-2"}>9</span>
                     <ToggleSwitch
@@ -53,7 +54,7 @@ const Page = () => {
                 </div>
             </section>
 
-            <section className={"px-8"}>
+            <section className={"px-4"}>
                 <TabWidget tabs={tabs}/>
             </section>
 
@@ -84,7 +85,7 @@ const ScoreCard = (props) => {
     const TotalPlayerScore = FrontNineScore + (isFullRound ? BackNineScore : 0);
 
     return (
-        <section className={"flex flex-col gap-2 justify-center items-start pt-8 pb-2.5"}>
+        <section className={"flex flex-col gap-2 justify-center items-end pb-4 mt-6"}>
             <div className={"grid grid-cols-10 gap-2"}>
                 {FrontNine.map((score, index) => {
                     const parValue = testGame.Holes[index].Par;
@@ -103,9 +104,9 @@ const ScoreCard = (props) => {
                     return (
                         <div key={index}>
                             <div className={"text-center font-semibold bg-lightBlue p-2 rounded-t-lg"}>{index + 1}</div>
-                            <div className={"flex"}>
-                                <div className={"p-2 bg-grey-1 rounded-bl-lg w-fit flex items-center justify-center text-center"}>{parValue}</div>
-                                <div className={`p-2 ${bgColor} rounded-br-lg text-black w-fit h-16 flex items-center justify-center text-center`}>{strokeValue}</div>
+                            <div className={"grid grid-cols-2 grow"}>
+                                <div className={"p-2 bg-grey-1 rounded-bl-lg flex items-center justify-center text-center"}>{parValue}</div>
+                                <div className={`p-2 ${bgColor} rounded-br-lg text-black flex items-center justify-center text-center`}>{strokeValue}</div>
                             </div>
                         </div>
                     );
@@ -114,7 +115,7 @@ const ScoreCard = (props) => {
                 <div>
                     <div className={"text-center font-semibold bg-blue p-2 rounded-t-lg"}>Out</div>
                     <div className={"flex"}>
-                        <div className={"p-2 bg-grey-2 text-black font-semibold text-2xl rounded-bl-lg rounded-br-lg w-28 h-16 flex items-center justify-center text-center"}>{FrontNineScore}</div>
+                        <div className={"p-2 bg-grey-2 text-black font-semibold text-xl rounded-bl-lg rounded-br-lg w-28 flex items-center justify-center text-center"}>{FrontNineScore}</div>
                     </div>
                 </div>
 
@@ -139,9 +140,9 @@ const ScoreCard = (props) => {
                         return (
                             <div key={index}>
                                 <div className={"text-center font-semibold bg-lightBlue p-2 rounded-t-lg"}>{index + 10}</div>
-                                <div className={"flex"}>
-                                    <div className={"p-2 bg-grey-1 rounded-bl-lg w-fit h-16 flex items-center justify-center text-center"}>{parValue}</div>
-                                    <div className={`p-2 ${bgColor} rounded-br-lg text-black w-fit h-16 flex items-center justify-center text-center`}>{strokeValue}</div>
+                                <div className={"grid grid-cols-2 grow"}>
+                                    <div className={"p-2 bg-grey-1 rounded-bl-lg  flex items-center justify-center text-center"}>{parValue}</div>
+                                    <div className={`p-2 ${bgColor} rounded-br-lg text-black flex items-center justify-center text-center`}>{strokeValue}</div>
                                 </div>
                             </div>
                         );
@@ -150,14 +151,14 @@ const ScoreCard = (props) => {
                     <div>
                         <div className={"text-center font-semibold bg-blue p-2 rounded-t-lg"}>In</div>
                         <div className={"flex"}>
-                            <div className={"p-2 bg-grey-2 text-black font-semibold text-2xl rounded-bl-lg rounded-br-lg w-28 h-16 flex items-center justify-center text-center"}>{BackNineScore}</div>
+                            <div className={"p-2 bg-grey-2 text-black font-semibold text-xl rounded-bl-lg rounded-br-lg w-28 flex items-center justify-center text-center"}>{BackNineScore}</div>
                         </div>
                     </div>
 
                 </div>
             )}
 
-            <div className="flex justify-end w-full px-[118px] mt-4 text-2xl italic"><span className="mr-4">Total Score : </span><span className="font-semibold">{TotalPlayerScore}</span></div>
+            <div className="flex justify-end w-full pr-8 mt-2 text-base"><span className="mr-4">Total Score : </span><span className="font-semibold">{TotalPlayerScore}</span></div>
         </section>
     );
 
