5e4d28af8dea50eb3a4dd22ad54df2ff01a296c3
Brian Tweed <brian.tweed@ioausa.com>
2025-12-11 19:11:57 +0000

    init commit

--- src/components/Leaderboard/index.js
+++ src/components/Leaderboard/index.js
@@ -7,7 +7,7 @@ const Leaderboard = () => {
     const players = testGame.Players;
 
 
-    function rankGolfScores(players, par = 72) {
+    function calculateLeaderboardData(players, par = 72) {
         return players
             .map((player) => {
                 const total = player.Scores.reduce((sum, hole) => sum + hole.Stroke, 0);
@@ -25,6 +25,7 @@ const Leaderboard = () => {
                 return {
                     Name: player.Name,
                     Handicap: player.Handicap,
+                    Scores: player.Scores.map((hole) => hole.Stroke),
                     Total: total,
                     RelativeToPar: relativeToPar
                 };
@@ -35,7 +36,8 @@ const Leaderboard = () => {
             });
     }
 
-    const leaderboardResults = rankGolfScores(players);
+    const leaderboardData = calculateLeaderboardData(players);
+
 
     return (
         <div className={styles.contents}>
@@ -43,15 +45,36 @@ const Leaderboard = () => {
                 <div>#</div>
                 <div>Name</div>
                 <div className={"text-center"}>Total</div>
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+                <div>4</div>
+                <div>5</div>
+                <div>6</div>
+                <div>7</div>
+                <div>8</div>
+                <div>9</div>
+                <div>10</div>
+                <div>11</div>
+                <div>12</div>
+                <div>13</div>
+                <div>14</div>
+                <div>15</div>
+                <div>16</div>
+                <div>17</div>
+                <div>18</div>
                 <div className={"text-right pr-1"}>Score</div>
             </div>
             <div className={styles.standings}>
-                {leaderboardResults.map((player, index) => {
+                {leaderboardData.map((player, index) => {
                     return (
                         <Fragment key={index}>
                             <div><i>{index+1}</i></div>
                             <div className={"font-semibold"}>{player.Name}</div>
                             <div className={styles.total}>{player.Total}</div>
+                            {player.Scores.map((score, index) => {
+                                return <div key={index}>{score}</div>;
+                            })}
                             <div className={styles.par}><div>{player.RelativeToPar}</div></div>
                         </Fragment>
                     );
