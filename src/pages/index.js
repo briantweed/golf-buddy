048e97fead2d14f99640f78866efde3c94c0463f
Brian Tweed <brian.tweed@ioausa.com>
2025-12-11 16:40:15 +0000

    init commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -70,6 +70,12 @@ const Page = () => {
                             "content": <div>{renderedForm}</div>
                         },
                         {
+                            "label": "Scorecard",
+                            "content": (
+                                <div>Rotate your phone to Landscape</div>
+                            )
+                        },
+                        {
                             "label": "Danger Zone",
                             "content": <div className={"flex flex-col items-start gap-4"}>
                                 <button type="button" className={"bg-red-2 p-2 px-4 pr-5 text-sm rounded-xl flex items-center gap-3"}><span className="text-xs">x</span> Clear Player Scores</button>
