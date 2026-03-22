b9ba6840ee8c5000c0606348f407e989d32bd8a2
Brian Tweed <brian.tweed@ioausa.com>
2026-03-22 18:11:49 +0000

    init commit

--- src/components/screens/Menu/Settings/index.js
+++ src/components/screens/Menu/Settings/index.js
@@ -2,14 +2,18 @@ import form from "@files/forms/setup";
 import validationRules from "@files/validation/setup.zod";
 import useFormBuilder from "@libraries/FormBuilder/useFormBuilder";
 import useLocalStorage from "@hooks/useLocalStorage";
+import {useEffect, useState} from "react";
 
 
 const Settings = () => {
 
     const {settings, updateSettings} = useLocalStorage();
 
+    const [display, setDisplay] = useState(false);
+
     const handleSubmit = (data) => {
         updateSettings(data);
+        setDisplay(true);
     };
 
     const renderedForm = useFormBuilder(form, validationRules, handleSubmit, {
@@ -17,8 +21,22 @@ const Settings = () => {
     });
 
 
+    useEffect(() => {
+        if (display) {
+            setTimeout(() => {
+                setDisplay(false);
+            }, 1000)
+        }
+    })
+
+
     return (
-        <div className={"bg-darkestGreen p-4 rounded-md"}>{renderedForm}</div>
+        <div className={"bg-darkestGreen p-4 rounded-md relative"}>
+            {renderedForm}
+            {display && (
+                <div className="absolute right-5 bottom-1.5 text-sm text-pastel-green-1  italic">&#10003; updated</div>
+            )}
+        </div>
     );
 
 };
