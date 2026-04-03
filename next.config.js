9240d4330734ccaa7ca41428c8f31f9b748d2e42
Brian Tweed <brian.tweed@ioausa.com>
2026-04-03 23:36:30 +0100

    g

---
+++ next.config.js
@@ -0,0 +1,49 @@
+import * as pwa from "@ducanh2912/next-pwa";
+const {join} = require("path");
+
+
+const moduleExports = {
+    reactStrictMode: true,
+    devIndicators: false,
+    output: "standalone",
+    env: {
+        VERSION: "0.0.1",
+        TITLE: "Golf Buddy",
+        SUBTITLE: "Scorecard",
+        LOCALE_STRING: "en-GB",
+        DEFAULT_TIMEZONE: "Europe/London"
+    },
+    sassOptions: {
+        includePaths: [join(__dirname, "styles")],
+        implementation: "sass-embedded",
+        quietDeps: true,
+        silenceDeprecations: ["legacy-js-api"]
+    },
+    images: {
+        loader: "custom",
+        loaderFile: "./imageLoader.js"
+    },
+    webpack: (config) => {
+        config.module.rules.push(
+            {
+                test: /\.md$/,
+                type: "asset/source",
+            }
+        );
+        return config;
+    }
+};
+
+const withPWA = pwa.default({
+    dest: 'public',
+    cacheOnFrontEndNav: true,
+    aggressiveFrontEndNavCaching: true,
+    reloadOnOnline: true,
+    swcMinify: true,
+    fallback: {
+        document: "/offline"
+    }
+});
+
+
+module.exports = withPWA(moduleExports);
