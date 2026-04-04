8ec0416ec4d7055ab7d6e76e43f903e47b9b9b3c
Brian Tweed <brian.tweed@ioausa.com>
2026-04-04 01:28:34 +0100

    g

--- src/files/config/courses.js
+++ src/files/config/courses.js
@@ -1,22 +1,25 @@
 const courses = [
 
     {
-        label: "Kilrea Golf Club",
-        value: "Kilrea",
-        Holes: [4,3,4,4,3,4,3,4,5,4,3,4,4,3,4,3,4,5]
-    },
-
-    {
         label: "Castlerock Golf Club",
         value: "Castlerock",
         Holes: [3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3]
     },
-
+    {
+        label: "Gracehill",
+        value: "Gracehill",
+        Holes: [4,4,4,5,4,4,3,4,5,4,3,5,4,3,5,3,4,4]
+    },
+    {
+        label: "Kilrea Golf Club",
+        value: "Kilrea",
+        Holes: [4,3,4,4,3,4,3,4,5,4,3,4,4,3,4,3,4,5]
+    },
     {
         label: "The Manor",
         value: "TheManor",
         Holes: [3,4,4,4,3,5,4,4,5,4,5,3,4,3,5,3,4,5]
-    },
+    }
 
 ];
 
